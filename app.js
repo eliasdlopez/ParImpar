21f40dee5e6b46f9199988cf2570ccd61a79d88c
Elias <eliasdamianlopez@gmail.com>
2020-04-13 20:02:53 -0500

    primer commit

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+
+var string = "";
+
+for (let i = 1; i <= 50; i++) {
+  if (i % 2 === 0) {
+    string += ('<p>' + i + ' Soy Impar!' + '</p>')
+  } else {
+    string += ('<p>' + i + ' Soy Par!' + '</p>')
+  }
+}
+
+app.get('/', (req, res) => {
+
+  res.send(string);
+});
+
+app.listen(3000, () => console.log('Listening on port 3000!'));
